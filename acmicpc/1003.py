990a5f7ad56c27535e1fc7bede73b0ba717f04c0
CODe1995 <code.57x53@gmail.com>
2020-03-03 00:10:22 +0900

    2020.03.03

--- acmicpc/1003.py
+++ acmicpc/1003.py
@@ -1,15 +1,15 @@
 def fibonacci(n):
-    if cn[n]!=[0,0]:
-        return cn[n]
-    
-    return cn[n]
+    if len(cn)<=n:
+        for i in range(len(cn),n+1):
+            cn.append([cn[i-1][0]+cn[i-2][0],cn[i-1][1]+cn[i-2][1]])
+    print(cn[n][0],cn[n][1])
 T=int(input())
 global cn
-cn=[[0,0]*42]
-cn[0]=[1,0]
-cn[1]=[0,1]
-cn[2]=[1,1]
+cn=[]
+cn.append([1,0])
+cn.append([0,1])
+cn.append([1,1])
 for i in range(T):
     n=int(input())
-    print(fibonacci(n))
+    fibonacci(n)
     
