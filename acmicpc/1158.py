379784ff905eeff79f251bc68d03330dc8e76663
CODe1995 <code.57x53@gmail.com>
2020-03-05 23:06:56 +0900

    2020.03.05

--- acmicpc/1158.py
+++ acmicpc/1158.py
@@ -1,5 +1,19 @@
 N,K=map(int,input().split())
 lists = list(range(1,N+1))
-while len(lists)>0:
-    print(lists[K-1])
-    del lists[K-1]
+stk = []
+pos=K-1
+while lists:
+    if len(lists)>pos:
+        stk.append(lists.pop(pos))
+        pos += K-1  #하나씩 pop 되니까 -1 해줘야함
+    else:#pos가 범위보다 커지면
+        pos = pos % len(lists)
+        stk.append(lists.pop(pos))
+        pos += K-1
+print('<',end='')
+for i,d in enumerate(stk):
+    print(d,end='')
+    if len(stk)>i+1:
+        print(', ',end='')    
+print('>')
+
