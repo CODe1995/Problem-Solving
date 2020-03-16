1557aa3fb91585bf6aa0418f000b1ce628019800
CODe1995 <code.57x53@gmail.com>
2020-03-16 23:07:55 +0900

    2020.03.16

--- acmicpc/1654.py
+++ acmicpc/1654.py
@@ -1,16 +1,17 @@
+import sys
 k,n=map(int,input().split())
-cables = []
-for _ in range(k):
-    cables.append(int(input()))
-cables = sorted(cables,reverse=True)
+cables = [int(sys.stdin.readline()) for _ in range(k)]
+# cables = sorted(cables,reverse=True)
 # print(cables)
-right = cables[0]
+right = max(cables)
 left = 0
 while left<=right:    
-    cnt=0
+    cnt=0    
     mid = (right+left)//2
+    if mid ==0:#0으로 나누는 모듈러 에러 방지
+        break
     for i in cables:
-        cnt += i//mid
+        cnt += i//mid        
     if cnt >= n:#갯수를 줄여야함 = 길이를 늘려야함
         left = mid + 1
     else:
