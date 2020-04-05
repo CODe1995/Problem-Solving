ffa5ae85db99c543d237de6dcf98174247107ffa
CODe1995 <code.57x53@gmail.com>
2020-04-05 23:46:34 +0900

    func

--- acmicpc/1509.py
+++ acmicpc/1509.py
@@ -1,16 +1,22 @@
 # arr = input()
-arr= list('BBCDDECAECBDABADDCEBACCCBDCAABDBADD')
+arr= list('ACAA')
 num = len(arr)+1
 dp = [2600]*num
 dp[0]=0
 field = [[0]*num for _ in range(num)]
 
 if num-1 == 1:print(1)#문자열 길이 1개
+
+for i in range(1,num):#우하향대각 모두 1
+    field[i][i]=1
+
 for i in range(1,num):
     for j in range(1,num):
         if arr[i-1] == arr[j-1]:
             field[i][j]=1
-print(field)
+
+print(i for i in field)
+
 for i in range(1,num):    
     for j in range(i,num):
         if field[i][j]==1:               
