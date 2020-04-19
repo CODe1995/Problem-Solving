bbd906df7d5b1689a9abf4e39845fd4d4b2d46a1
CODe1995 <code.57x53@gmail.com>
2020-04-19 23:50:07 +0900

    재풀이중

--- acmicpc/1018.py
+++ acmicpc/1018.py
@@ -1,18 +1,41 @@
 n,m = map(int,input().split())
 board = [list(input()) for _ in range(n)]
-cnt=[0,0]
-for cost in range(2):
-    if cost==0:turn='B'
-    else: turn='W'
-    for i in range(n):
-        for j in board[i]:
-            if cost==0:#BW
-                if j != turn:       
-                    cnt[cost]+=1
-            elif cost==1:#WB
-                if j != turn:       
-                    cnt[cost]+=1
+boardW = [[0]*m for _ in range(n)]
+boardB = [[0]*m for _ in range(n)]
+
+flag = 'W'
+for i in range(n):
+    for j in range(m):
+        if board[i][j]==flag:
+            boardW[i][j]=0
+            boardB[i][j]=1
+        else:
+            boardW[i][j]=1
+            boardB[i][j]=0
+        flag = 'W' if flag=='B' else 'B'        
+    flag = 'W' if flag=='B' else 'B'
+for i in range(n):
+    print(boardW[i])
+print()
+for i in range(n):
+    print(boardB[i])
+# for i in range(n):
+#     for j in range(m):
+#         if j+8<=m and i+8<=n:
+#             print(i,j,'==')
+#             # print(boardW[j:j+8][i:i+8])
+#             # print(boardB[j:j+8][i:i+8])
+
+#             for w in boardW[i:i+8]:
+#                 print(sum(w[j:j+8]),end=':')
+#                 for inw in w[j:j+8]:
+#                     print(inw,end=' ')
+
+#                 print()
+#             print('B')
             
-            if turn=='B':turn='W'
-            else: turn='B'
-print(cnt)
+#             for w in boardB[i:i+8]:
+#                 print(sum(w[j:j+8]),end=':')
+#                 for inw in w[j:j+8]:
+#                     print(inw,end= ' ')
+#                 print()
