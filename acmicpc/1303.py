1b17a81fd06a584685a2113d7f3d3c9d5b456d7c
CODe1995 <code.57x53@gmail.com>
2020-04-21 01:10:26 +0900

    1303 bfs? dfs? 해결
    생각보다 단순했고 재귀함수를 이용했다.

--- acmicpc/1303.py
+++ acmicpc/1303.py
@@ -1,27 +1,32 @@
-def bfs(field,width,height):
-    sumFoes = 0 #적군의 합
-    sumAllies = 0 #아군의 합
-    visited=[[0]*width for _ in range(height)]
-    
-    nextq=[]
-    nextq.append((0,0))
-    move=[(0,1),(1,0),(-1,0),(0,-1)]
-    while nextq:
-        dx,dy = nextq.pop(0)
-        for x,y in move:#다음 방문지 선정
-            if dx+x >= 0 and dx+x <width and dy+y>=0 and dy+y<height and visited[dx+x][dy+y]==0:
-                nextq.append((dx+x,dy+y))#다음 방문지를 nextq에 넣는다.
-        
-        
-            
-        
-        
-        
-
 N,M = map(int,input().split())  #가로 N 세로 M
 field = []
+visited=[[0]*N for _ in range(M)]
+move=[(0,1),(1,0),(-1,0),(0,-1)]
+sumFoes = 0 #적군의 합
+sumAllies = 0 #아군의 합
+cnt=0
 for _ in range(M):
     field.append(list(map(str,input())))
 
-print(bfs(field,N,M))
-    
+def bfs(h,w,who):    
+    global cnt
+    visited[h][w]=1
+    for (dx,dy) in move:#다음 방문지 선정
+        if w+dx >= 0 and w+dx <N and h+dy>=0 and h+dy<M:
+            if visited[h+dy][w+dx]==0 and field[h+dy][w+dx]==who:
+                bfs(h+dy,w+dx,who)
+    cnt+=1
+
+for h in range(M):#M은세로
+    for w in range(N):#N은가로
+        if visited[h][w]==1:
+            continue
+        bfs(h,w,field[h][w])
+        res= cnt
+        if field[h][w]=='W':
+            sumAllies+=res**2
+        else:
+            sumFoes+=res**2
+        cnt=0
+
+print(sumAllies,sumFoes)
