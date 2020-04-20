b4ce7173aecda9e907ff26bc345ec61c906937b4
CODe1995 <code.57x53@gmail.com>
2020-04-20 23:49:24 +0900

    1303 재시도중

--- acmicpc/1303.py
+++ acmicpc/1303.py
@@ -1,21 +1,23 @@
 def bfs(field,width,height):
-    visited=[]
+    sumFoes = 0 #적군의 합
+    sumAllies = 0 #아군의 합
     visited=[[0]*width for _ in range(height)]
-    visited[0][0]=field[0][0]
-    next_q = []
-    next_q.append((0,0))
-    dx = [0,0,1,-1]
-    dy = [1,-1,0,0]
-    sumen = 0 #적군합
-    sumur = 0 #아군합
-    while next_q:
-        bx,by = next_q.pop(0)
-        for i in range(4):
-            ax = bx + dx[i]
-            ay = by + dy[i]
-            if ax<width and ay<height and ax>=0 and ay>=0 and visited[ax][ay]==0:
-                if visited
-    return #우리팀 적팀 N^2
+    
+    nextq=[]
+    nextq.append((0,0))
+    move=[(0,1),(1,0),(-1,0),(0,-1)]
+    while nextq:
+        dx,dy = nextq.pop(0)
+        for x,y in move:#다음 방문지 선정
+            if dx+x >= 0 and dx+x <width and dy+y>=0 and dy+y<height and visited[dx+x][dy+y]==0:
+                nextq.append((dx+x,dy+y))#다음 방문지를 nextq에 넣는다.
+        
+        
+            
+        
+        
+        
+
 N,M = map(int,input().split())  #가로 N 세로 M
 field = []
 for _ in range(M):
