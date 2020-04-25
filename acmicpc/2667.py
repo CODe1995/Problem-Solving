7a8e10599f23bf5cf513df566e4c11c1eb031d08
unknown <code.57x53@gmail.com>
2020-04-26 08:24:45 +0900

    2667 dfs
    dfs로 풀어봤다. 여태 풀었던 방식은 bfs였는데 아직 구분은 잘 안된다.
    이론상 깊이 우선인지 너비 우선인지 구분은 가능하다.
    쉬운 난이도다.

---
+++ acmicpc/2667.py
@@ -0,0 +1,34 @@
+n = int(input())
+field = []
+direction = [[0,1],[1,0],[-1,0],[0,-1]]
+
+for i in range(n):
+    field.append(list(map(int,input())))
+
+visited = [[0]*n for _ in range(n)]
+
+def dfs(nx,ny,cnt):
+    visited[nx][ny]=1
+    for dx,dy in direction:
+        tx = nx+dx
+        ty = ny+dy
+        if tx>=0 and ty>=0 and tx<n and ty<n:
+            if visited[tx][ty]==0 and field[tx][ty]==1:
+                cnt=dfs(tx,ty,cnt+1)
+    return cnt
+ans = []
+for i in range(n):
+    for j in range(n):
+        if field[i][j]==1 and visited[i][j]==0:            
+            ans.append(dfs(i,j,0)+1)
+print(len(ans))
+for i in sorted(ans):
+    print(i)
+
+
+
+
+    
+
+
+
