9f8daffe3fdf87010a55a8df46f7a40ffb286add
unknown <code.57x53@gmail.com>
2020-04-30 23:40:23 +0900

    7576 토마토 bfs 코드단축
    1. bfs 함수 내부 nx와 ny의 조건 단축
    2. bfs 함수 내부 bool을 이용한 출력이 아닌 함수 return 이용

--- acmicpc/7576.py
+++ acmicpc/7576.py
@@ -3,43 +3,30 @@ width,height = map(int,input().split())
 direction = [[1,0],[0,1],[-1,0],[0,-1]]
 arr = []
 visited=[]
-afarr = []
 nextq = deque()
-cnt=0
 for i in range(height):
     arr.append(list(map(int,input().split())))
-    visited.append([0]*width)
+    visited.append([0]*width)    
     for j in range(width):   #입력과 동시에 토마토 위치를 따로 보관한다.
         if arr[i][j] == 1:
             nextq.append([j,i,0])
             visited[i][j]=1
 
-afarr = arr
-
 def bfs():        
     life=0
     while nextq:
         tmx,tmy,life=nextq.popleft()
-        afarr[tmy][tmx]=1
+        arr[tmy][tmx]=1
         for dx,dy in direction:
             nx = tmx+dx
             ny = tmy+dy
-            if nx >=0 and ny>=0 and nx<width and ny<height:
-                if afarr[ny][nx]==0 and visited[ny][nx]==0:
+            if 0<=nx<width and 0<=ny<height:
+                if arr[ny][nx]==0 and visited[ny][nx]==0:
                     nextq.append([nx,ny,life+1])
-                    visited[ny][nx]=1
-    pos = True
+                    visited[ny][nx]=1    
     for i in range(height):
-        for j in range(width):
-            if afarr[i][j]==0:
-                pos = False
-                break      
-        if pos==False:
-            print(-1)
-            break
-    if pos == True:      
-        print(life)
+        if 0 in arr[i]:
+            return -1
+    return life
 
-bfs()
-
-
+print(bfs())
