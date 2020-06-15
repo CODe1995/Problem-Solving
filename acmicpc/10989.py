d4c92944886904248968afbf57649c14d30b2a96
unknown <code.57x53@gmail.com>
2020-06-15 22:11:47 +0900

    10989 정렬
    메모리에 제한이 걸려 있는데, 단순 정렬은 크게 문제가 되지 않는다.
    어떻게 메모리를 효율적으로 사용할 것인가가 중요했는데
    천만개의 배열을 선언했다가 계속 메모리 초과가 나길래
    최소 입력값 1부터 최대 1만까지의 총 1만1개의 배열의 방을 생성했고
    갯수만큼 카운트 시켜줬다..
    발상의 전환.. 한 수 배웠다

--- acmicpc/10989.py
+++ acmicpc/10989.py
@@ -1,7 +1,13 @@
-n = int(input())
-arr = []
-while n:
-    n-=1
-    arr.append(int(input()))
-for i in sorted(arr):
-    print(i)
+import sys
+input = sys.stdin.readline
+n = int(input().rstrip())
+arr = [0]*10001
+
+for i in range(n):
+    arr[int(input())]+=1
+
+for i in range(10001):
+    if arr[i] != 0 :
+        for j in range(arr[i]):
+            print(i)
+        
