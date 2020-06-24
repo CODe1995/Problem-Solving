9bf3ee1200c15bf9d6f27986012b3fa7aae5d4f0
unknown <code.57x53@gmail.com>
2020-06-24 22:36:17 +0900

    1024 오답

---
+++ acmicpc/1024.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+N, L = map(int,input().split())
+ansarr = []
+arrsum = 0
+def fibo(num):
+    if num<2:
+        return num
+    else:
+        return num+fibo(num-1)
+
+while L<=100:
+    x = (N-fibo(L))/L
+    y = (N-fibo(L))%L
+    if y == 0 and x>=0:
+        for ans in range(1,L+1):
+            print(int(x+ans), end = ' ')
+        break
+    else:
+        L+=1
+if L>100:print(-1) 
