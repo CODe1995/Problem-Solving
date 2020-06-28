8e1f4f3ea1142fc92e3eb1c7119c9e4ba7245d4c
unknown <code.57x53@gmail.com>
2020-06-28 22:57:34 +0900

    O(n) 다시 수정할것

--- acmicpc/1182.py
+++ acmicpc/1182.py
@@ -1,7 +1,19 @@
 N,S = map(int,input().split())
 arr = list(map(int,input().split()))
 arr.sort()
+
+def dynamic_programming(arrs):
+    cache = [None] * len(arrs)
+    cache[0] = arr[0]
+
+    for i in range(1,len(arr)):
+        cache[i] = max(0,cache[i-1])+arr[i]
+    
+    return max(cache)
+
+
 if len(arr)>1:
     for i in range(1,len(arr)):
         arr[i] = arr[i-1]+arr[i]
 print(arr)
+print(dynamic_programming(arr))
