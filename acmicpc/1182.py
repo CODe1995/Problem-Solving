69f548831b7b723602f85f3ce0da73f713fee9fb
CODe1995 <code.57x53@gmail.com>
2020-07-01 17:02:59 +0900

    1182 부분수열의합 정답
    계속 틀렸던 이유는 연속된 부분수열이라고 생각을 했고
    계속해서 prefix sum으로 해결하려 했다.
    하지만 해당 문제에서는 prefix가 아닌 위치에 전혀 상관 없이 합이
    S와 일치하면 되는 문제였기에 dfs로 전수 조사를 실시했다.

--- acmicpc/1182.py
+++ acmicpc/1182.py
@@ -1,37 +1,15 @@
-arr = [None]
-changepos = 0
-def main():
-    global arr, changepos
-    N,S = map(int,input().split())
-    arr = list(map(int,input().split()))
-    arr.sort()
-    cnt = 0
-    changepos = 0
-    for i in range(1,len(arr)):
-        arr[i] += arr[i-1]
-        if arr[i]>arr[i-1] and changepos==0:changepos=i
+cnt = 0
+N,S = None,None
 
-    start = 1
-    end = changepos    
+def dfs(i, su):
+    global cnt
+    if i==N: return
+    if su+arr[i] == S:
+        cnt+=1
+    dfs(i+1,su)    
+    dfs(i+1,su+arr[i])
 
-    while start<=end and end<len(arr):
-        presum = prefix(start,end)        
-        if presum == S:
-            cnt+=1
-            start+=1
-        elif presum < S:
-            end+=1
-        else:
-            start+=1
-    # print(arr,changepos)
-    print(cnt)
-
-def prefix(t,f):    #to, from
-    # print(t-1,f,arr[t-1]-arr[f])
-    if changepos >1:
-        return -arr[f]+arr[t-1]
-    else:
-        return arr[f]-arr[t-1]
-
-if __name__ == "__main__":
-    main()
+N,S = map(int,input().split())
+arr = list(map(int,input().split()))
+dfs(0,0)
+print(cnt)
