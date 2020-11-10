4f98b1c3a95abe7de6fd822431e0c8e9f583d0ba
CODe1995 <code.57x53@gmail.com>
2020-11-10 13:58:07 +0900

    '완료'
    시간초과 이유는 dictionary를 활용한게 문제인듯.
    조건 비교 not in dictionary를 해서 매 값 삽입마다 검색이 일어나서
    시간초과가 생긴것 같아서 딕셔너리를 리스트로 바꾸고 별다른 조건 검사
    없이 바로 값을 추가하게끔 바꿈

--- acmicpc/11724.py
+++ acmicpc/11724.py
@@ -1,6 +1,6 @@
 import sys
 sys.setrecursionlimit(10000)#재귀 제한
-N,M = map(int,input().split())
+N,M = map(int,sys.stdin.readline().split())
 
 def DFS(n):
     visited[n]=True
@@ -9,23 +9,16 @@ def DFS(n):
             DFS(v)    
 visited= []
 visited = [False]*(N+1)
-graph = {}
+graph = [[] for _ in range(N+1)]
 
 for _ in range(M):
-    n1,n2 = map(int,input().split())
-    if n1 not in graph:
-        graph[n1]=[n2]
-    elif n2 not in graph[n1]:
-        graph[n1].append(n2)
-    if n2 not in graph:
-        graph[n2]=[n1]
-    elif n1 not in graph[n2]:
-        graph[n2].append(n1)
+    n1,n2 = map(int,sys.stdin.readline().split())
+    graph[n1].append(n2)
+    graph[n2].append(n1)
 
 cnt = 0
-for i in graph:
+for i in range(1,N+1):
     if not visited[i]:#visited[i]가 False일 때(방문 안했을 때)
         DFS(i)
         cnt+=1
-print(cnt)
-    
+sys.stdout.write(str(cnt))    
