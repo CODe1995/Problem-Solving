be7bb25a12ab4d582de370bc1291272fa0f2e7bc
CODe1995 <code.57x53@gmail.com>
2020-11-11 17:14:17 +0900

    '통과'
    BFS로 풀었는데 생각해보니 visited를 따로 관리할 필요 없이 field 1을
    방문했으면 0으로 바꿔주면 끝이었음.
    DFS로 바꿨고 한번 방문마다 끝까지 탐색하기 때문에 방문한 field는 0으로
    바꿔주고 다음번에 다시 방문 안하게 바꿔줌

--- acmicpc/1012.py
+++ acmicpc/1012.py
@@ -1,31 +1,30 @@
 import sys
 from collections import deque
-def BFS(root):    #visited 반환
-    direction = [[1,0],[0,1],[-1,0],[0,-1]]
-    n = deque([root])   #다음 방문지        
-    while n:
-        x,y = n.popleft() #방문한 곳 제거, 좌표 저장
-        visited[y][x] = 1
-        for dx,dy in direction:
-            if 0<= y+dy < len(field) and 0<=x+dx < len(field[0]):
-                # 배추가 심어져있고, 아직 방문 안한경우
-                if field[y+dy][x+dx]==1 and visited[y+dy][x+dx]==0:
-                    n.append([x+dx,y+dy])  
-
+sys.setrecursionlimit(100000)
+direction = [[1,0],[0,1],[-1,0],[0,-1]]
+def DFS(root):    #visited 반환
+    _x,_y = root
+    field[_y][_x] = 0
+    for dx,dy in direction:
+        nx = _x+dx
+        ny = _y+dy
+        if 0<= ny < len(field) and 0<=nx < len(field[0]):
+            # 배추가 심어져있으면
+            if field[ny][nx]==1:
+                DFS([nx,ny])
 
 T = int(sys.stdin.readline())
 for _ in range(T):
     stack = []  #배추가 있는 곳 저장        
     M,N,K = map(int,sys.stdin.readline().split())  #가로,세로,배추갯수
     field = [[0]*M for _ in range(N)]
-    visited = [[0]*M for _ in range(N)]    #방문한 곳 저장
     for _ in range(K):
         n1,n2 = map(int,sys.stdin.readline().split())
         stack.append([n1,n2])   #배추가 있는 좌표값 저장
         field[n2][n1] = 1
     cnt = 0
     for [x,y] in stack:
-        if visited[y][x] == 0:    #방문한 기록 없으면
-            BFS([x,y])    #방문
+        if field[y][x] == 1:    #배추가 심어져 있으면
             cnt+=1
+            DFS([x,y])    #방문
     print(cnt)
