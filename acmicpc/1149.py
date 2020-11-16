a0eb8e09dc11718b1f64f586a2d543cccb96c0de
CODe1995 <code.57x53@gmail.com>
2020-11-16 22:47:23 +0900

    통과

--- acmicpc/1149.py
+++ acmicpc/1149.py
@@ -1,4 +1,12 @@
-n = int(input())
+import sys
+input = sys.stdin.readline
+n = int(input().rstrip())
 arr = []
 for i in range(n):
-    arr.append(list(map(int,input().split())))
+    arr.append(list(map(int,input().rstrip().split())))
+
+for i in range(1,n):
+    arr[i][0] = min(arr[i-1][1],arr[i-1][2])+arr[i][0]
+    arr[i][1] = min(arr[i-1][0],arr[i-1][2])+arr[i][1]
+    arr[i][2] = min(arr[i-1][0],arr[i-1][1])+arr[i][2]
+print(min(arr[i]))
