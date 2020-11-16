f5b763e3c4c381428a1f428163a20460af8c77b1
CODe1995 <code.57x53@gmail.com>
2020-11-16 21:57:29 +0900

    수정중

--- acmicpc/2579.py
+++ acmicpc/2579.py
@@ -1,17 +1,28 @@
-n = int(input())
-arr = []
-for i in range(n):
-    arr.append(int(input()))
+import sys
+input = sys.stdin.readline
+  
+
+
+N = int(input().rstrip())
+# stairs_state = [[0] for _ in range(N-1)]
+stairs = dict()
+scores_state = []
+for i in range(N):
+    scores_state.append(int(input().rstrip()))
 cnt = 0
 
-#총 계단 수, 현재 밟고 있는 계단, 현재 점수
-def calc(maxS,now,points):
-    if maxS-1 == now:  #도착했다면
-        return arr[max-1]
-    if maxS-1 - now >= 1:
-        points = points + max(calc(maxS,i+1,points),calc(maxS,i+2,points))  #한 칸과 두 칸 올라가는 점수 비교
-        
+root = []
+#경로를 구함
+
+solution([0],0,True)
+#현재까지 밟아온 계단, 몇번째 배열인지 idx, 연속건너기 가능한지 가능하면T 불가능F
+def solution(current,idx,successive=True):
+    if current[-1] == N: return current
+    if successive == True and N-2>=current[-1]:  #연속밟기
+        current.append(current[-1]+1)   #마지막계단 +1
+        successive=False    #연속밟기 불가능하게 바꿈
+        solution(current,idx,jump)
+    else if
 
     
 
-calc(n,0,0)
