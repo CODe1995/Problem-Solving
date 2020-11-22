84c24590ef8616eb97a89ac362a2939bedbafcdf
CODe1995 <code.57x53@gmail.com>
2020-11-22 18:56:54 +0900

    오답

--- acmicpc/2470.py
+++ acmicpc/2470.py
@@ -4,13 +4,14 @@ input = sys.stdin.readline
 N = int(input().rstrip())
 arr = list(map(int,input().rstrip().split()))
 arr.sort()
+print(arr)
 nL,nR=0,0
 num = 1000000001    #최대값
 left,right=0,len(arr)-1
 while left!=right:
-    tP = abs(arr[left]+arr[right])
-    if num>tP:
-        num=tP
+    tP = arr[left]+arr[right]
+    if num>abs(tP):
+        num=abs(tP)
         nL = arr[left]
         nR = arr[right]
         if num==0:
