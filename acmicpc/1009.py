406c5bd92afa581727825dc96815ea1a437386bd
CODe1995 <code.57x53@gmail.com>
2020-11-24 02:29:30 +0900

    1009 오답

--- acmicpc/1009.py
+++ acmicpc/1009.py
@@ -4,4 +4,6 @@ input = sys.stdin.readline
 T = int(input().rstrip())
 for _ in range(T):
     a,b = map(int,input().rstrip().split())
-    print(str(a**b)[-1])
+    b = int(str(b)[-1])
+    tmp = int(str(a**b)[-1] )
+    print(10 if tmp==0 else tmp)
