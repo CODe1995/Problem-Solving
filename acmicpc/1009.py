0446e7e990580c0d88b109ac0178974b36048378
CODe1995 <code.57x53@gmail.com>
2020-11-24 02:47:30 +0900

    1009 정답

--- acmicpc/1009.py
+++ acmicpc/1009.py
@@ -1,9 +1,31 @@
 import sys
 input = sys.stdin.readline
-
+dictionary = {
+    0:[10],
+    1:[1],
+    2:[2,4,8,6],
+    3:[3,9,7,1],
+    4:[4,6],
+    5:[5],
+    6:[6],
+    7:[7,9,3,1],
+    8:[8,4,2,6],
+    9:[9,1]
+}
 T = int(input().rstrip())
 for _ in range(T):
     a,b = map(int,input().rstrip().split())
-    b = int(str(b)[-1])
-    tmp = int(str(a**b)[-1] )
-    print(10 if tmp==0 else tmp)
+    print(dictionary[int(str(a)[-1])][b%len(dictionary[int(str(a)[-1])])-1])
+
+'''
+0 10
+1 1
+2 2 4 8 6
+3 3 9 7 1
+4 4 6
+5 5
+6 6
+7 7 9 3 1
+8 8 4 2 6
+9 9 1
+'''
