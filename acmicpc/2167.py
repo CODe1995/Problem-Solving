2ecdfd772bc39624db8c143c137f770d7a28e67f
CODe1995 <code.57x53@gmail.com>
2020-11-25 15:45:17 +0900

    2167 통과

--- acmicpc/2167.py
+++ acmicpc/2167.py
@@ -3,39 +3,15 @@ input = sys.stdin.readline
 
 N,M = map(int,input().rstrip().split())
 array = []
-for _ in range(N):
-    tmp = list(map(int,input().rstrip().split()))
-    array.append(tmp)
-for x in range(M):
-    for y in range(N):
-        if x==0 and y==0:
-            pass
-        elif y==0 and x>=1:
-            array[y][x]+=array[N-1][x-1]
-        else:
-            array[y][x]+=array[y-1][x]
-K=int(input())
+dp = [[0]*(M+1) for _ in range(N+1)]
+for i in range(1,N+1):
+    array.append(list(map(int,input().rstrip().split())))
+for y in range(1,N+1):
+    for x in range(1,M+1):
+        dp[y][x] = array[y-1][x-1] + dp[y][x-1] + dp[y-1][x]-dp[y-1][x-1]
 
-#i j x y
+K = int(input())
 for _ in range(K):
-    sY,sX,eY,eX = map(int,input().rstrip().split())
-    sY-=1
-    sX-=1
-    eY-=1
-    eX-=1
-
-    if sX==0 and sY==0:
-        print(array[eY][eX])
-    elif sX>=1 and sY==0:
-        print(array[eY][eX]-array[N-1][sX-1])
-    else:
-        print(array[eY][eX]-array[sY-1][sX])
-    # if i-1==0:
-    #     if j-1>=1:
-    #         j-=1
-    #         i=M-1
-    #         print(array[x][y]-array[i][j])
-    #     else:
-    #         print(array[x][y])
-    # else:        
-    #     print(array[x][y]-array[i-1][j])
+    i,j,x,y = map(int,input().rstrip().split())
+    answer = dp[x][y] - dp[x][j-1] - dp[i-1][y] + dp[i-1][j-1]
+    print(answer)
