f5ea71698614beb04e120f16a0157370081c47eb
CODe1995 <code.57x53@gmail.com>
2020-11-25 16:26:32 +0900

    1173 통과

--- acmicpc/1173.py
+++ acmicpc/1173.py
@@ -4,15 +4,21 @@ N,m,M,T,R = map(int,input().split())
 cur_m = m   #초기 맥박
 n = 0   #현재 운동한 시간
 cnt = 0 #흐른 시간
-while n<N:
-    while cur_m+T <= M and n<N:
-        cur_m+=T
-        n+=1
-        cnt+=1
-        # print('운동',n,cnt,cur_m)
-    if cur_m-R >=m and n<N:
-        cur_m-=R
-        cnt+=1
-        # print('휴식',n,cnt,cur_m)
+if m+T > M or R==0: #불가능한경우
+    print(-1)
+else:
+    while n<N:
+        if cur_m+T <= M and n<N:
+            cur_m+=T
+            n+=1
+            cnt+=1
+            # print('운동',n,cnt,cur_m)
+        else:
+            if cur_m-R <m:
+                cur_m=m
+            else:
+                cur_m-=R
+            cnt+=1
+            # print('휴식',n,cnt,cur_m)
 
-print(cnt)
+    print(cnt)
