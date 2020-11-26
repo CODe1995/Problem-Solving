6e46308530391e38506d531e70fad1223bcccdaa
CODe1995 <code.57x53@gmail.com>
2020-11-27 02:53:30 +0900

    1236 통과

--- acmicpc/resolving/1236.py
+++ acmicpc/resolving/1236.py
@@ -1,26 +1,12 @@
 height,width = map(int,input().split())
 castle = []
-Xchecker_height = [0]*width
-Xchecker_width = [0]*height
 for _ in range(height):
     s=list(input())
     castle.append(s)
-cRow=0    #열
-cCol=0    #행
-cnt = 0
+cRow=[]
+cCol=[]
 for i in range(height):
-    if 'X' in castle[i]:
-        Xchecker[i]=1
-for i in range(height):
-    if 'X' not in castle[i]:
-        idx = 0
-        if 0 in Xchecker:
-            idx = Xchecker.index(0)
-            Xchecker[idx]=1
-        castle[i][idx]='X'        
-        cnt+=1
-
-    for k in castle:
-        print(k)
-    print()
-print(cnt)
+    cRow.append('X' not in castle[i])
+for j in range(width):
+    cCol.append('X' not in [castle[i][j] for i in range(height)])
+print(max(sum(cRow),sum(cCol)))
