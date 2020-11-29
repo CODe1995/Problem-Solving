61575d71b7f10d952d603b1f131be73e9a86cbec
CODe1995 <code.57x53@gmail.com>
2020-11-29 20:18:43 +0900

    1074 시간초과

--- acmicpc/accpeted/1074.py
+++ acmicpc/accpeted/1074.py
@@ -9,7 +9,7 @@ def solve(size, x, y):
             nx = dx+x
             ny = dy+y
             if nx==c and ny==r:
-                print(cnt)
+                print(int(cnt))
                 return
             cnt+=1
     else:
@@ -17,4 +17,15 @@ def solve(size, x, y):
         solve(size/2,x+size/2,y)#rightTop
         solve(size/2,x,y+size/2)#leftDown
         solve(size/2,x+size/2,y+size/2)#rightDown
-solve(2**n,0,0)
+def whrQuadrant(size,x,y):#좌표 입력하면 몇사분면인지 반환
+    if size/2<=x and size/2>y:#1사분면
+        return (size/2)**2, size/2,0
+    if size/2>x and size/2>y:#2사분면
+        return 0,0,0
+    if size/2>x and size/2<=y:#3사분면
+        return ((size/2)**2) * 2,0,size/2
+    if size/2<=x and size/2<=y:#4사분면
+        return ((size/2)**2) * 3,size/2,size/2
+size = 2**n
+cnt,x,y = whrQuadrant(size,c,r)
+solve(size,x,y)
