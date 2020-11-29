612157e1ef7210548d4a13eec7f79027d1d93897
CODe1995 <code.57x53@gmail.com>
2020-11-29 20:52:02 +0900

    1074 정답

--- acmicpc/accpeted/1074.py
+++ acmicpc/accpeted/1074.py
@@ -7,12 +7,16 @@ while True:
         break
     if size/2<=x and size/2>y:#1사분면
         cnt+=(size/2)**2
+        x-=size/2
     elif size/2>x and size/2>y:#2사분면
-        cnt+=size/2
+        cnt+=0
     elif size/2>x and size/2<=y:#3사분면
         cnt+=((size/2)**2) * 2
+        y-=size/2
     elif size/2<=x and size/2<=y:#4사분면
         cnt+=((size/2)**2) * 3
+        x-=size/2
+        y-=size/2
     size/=2
 
 print(int(cnt))
