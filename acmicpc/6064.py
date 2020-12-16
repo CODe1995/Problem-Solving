27c44afc7e12d37a85288091f341eab56d4b72f8
unknown <code.57x53@gmail.com>
2020-12-16 22:35:09 +0900

    수식 이해하고 넘어감

--- acmicpc/6064.py
+++ acmicpc/6064.py
@@ -1,13 +1,26 @@
 import sys
 from math import gcd
+input = sys.stdin.readline
+def ii():return int(input())
+def mii():return map(int,input().rstrip().split())
+def lmii():return list(map(int,input().rstrip().split()))
 
 def lcm(a,b):
-    return a*b//gcd(a,b)
+    r=gcd(a,b)
+    return r*a//r*b//r
 
-inp = sys.stdin.readline
-Case = int(input())
-datas = [list(map(int,input().split(' '))) for _ in range(Case)]
-flag = True
-
-# for M,N,x,y in datas:
-    
+t = ii()
+for _ in range(t):
+    m,n,x,y, = mii()
+    mx = lcm(m,n)
+    print('mx %d'%mx)
+    s,e = 0,0
+    cnt=0
+    while m*s+n*e<=mx-(x+y)  and s<=m and e<=n:        
+        e=0
+        while m*s+n*e<=mx-(x+y) and s<=m and e<=n:
+            cnt+=1
+            e+=1
+        s+=1
+        
+    print(s,e,cnt)
