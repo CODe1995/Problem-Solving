4dbec9950844661f0f5bc5d4606ec0090f7c783b
unknown <code.57x53@gmail.com>
2020-12-17 16:58:02 +0900

    WA

--- acmicpc/15560.py
+++ acmicpc/15560.py
@@ -1,46 +1,66 @@
+##########################################################
 import sys
 input = sys.stdin.readline
 def ii():return int(input())
 def mii():return map(int,input().rstrip().split())
 def lmii():return list(map(int,input().rstrip().split()))
-tree= list()
+##########################################################
+from math import ceil,log2
+MAXNUM = 10**2+1
 nodes = list()
 n,q,u,v = mii()
 nodes = lmii()
-tree = [0]*(n*4)
-def update(start,end,index,cidx,diff):
-    if index>end or index<start:return#범위밖
-    tree[index]+=diff
-    if start==end:return
-    mid = (start+end)//2
-    update(start,mid,index*2,cidx,diff)
-    update(mid+1,end,index*2+1,cidx,diff)
+nh = ceil(log2(n))
+ncnt = (1 << (nh+1))
+tree=[{
+    'ls' :-MAXNUM,#왼쪽 구간 최대합
+    'rs' : -MAXNUM,#우측 구간 최대합
+    'lrs' : -MAXNUM,#구간 전체(좌+우) 최대합
+    'mxs' : 0#좌측,우측,전체 최대합 중의 최대
+}]*ncnt
+#노드 두개를 연산함
+def calc(L,R):
+    tmp={
+            'ls' : max(L['ls'],L['lrs']+R['ls']),
+            'rs' : max(R['rs'],L['rs']+R['lrs']),
+            'lrs' : L['lrs']+R['lrs'],
+            'mxs' : max(L['mxs'],R['mxs'],L['rs']+R['ls']),
+            }
+    return tmp
     
 def query(start,end,index,left,right):
     if left>end or right < start:#범위밖
-        return 0
+        return tree[0]  #0번은 어차피 안쓰임
     if left<=start and end<=right:#범위내
-        return u*tree[index]+v*(end-start)
-    mid = (start+end)//2
-    res = query(start,mid,index*2,left,right)+query(mid+1,end,index*2+1,left,right)
-    return res
-
-def init(start,end,index):
-    if start==end:
-        tree[index] = nodes[start]
         return tree[index]
-    mid = (start+end)//2    
-    tree[index] = init(start,mid,index*2)+init(mid+1,end,index*2+1)
-    return tree[index]
+    mid = (start+end)//2
+    r1,r2 = query(start,mid,index*2,left,right), query(mid+1,end,index*2+1,left,right)    
+    return calc(r1,r2)
 
-init(0,n-1,1)
+#시작점, 끝점, 현재위치, 수정좌표위치, 수정될값
+def update(start,end,index,cidx,cnum):#init과 동일
+    if cidx>end or cidx<start:return
+    if start==end:
+        tree[index]={'ls' : cnum, 'rs' : cnum, 'lrs' : cnum, 'mxs' : cnum}        
+        return
+    mid = (start+end)//2        
+    update(start,mid,index*2,cidx,cnum)
+    update(mid+1,end,index*2+1,cidx,cnum)
+    tree[index] = calc(tree[index*2],tree[index*2+1])
+
+for i in range(n):#u와 v의 값이 반영된 쿼리 업데이트(init)
+    # print("init_updates(%d,%d,%d,%d,%d)"%(0,n-1,i,1,u*nodes[i]+v))
+    update(0,n-1,1,i,u*nodes[i]+v)
+
+# for i in range(0,ncnt):
+#     print(tree[i])
+
 for _ in range(q):
     c,a,b = mii()
     if c == 0:#첫번째쿼리 max
-        res = query(0,n-1,1,a-1,b-1)
-        print(res)
+        print(query(0,n-1,1,a-1,b-1)['mxs']-v)
     else:#두번재 쿼리 update
-        diff = b-nodes[a-1]
         nodes[a-1]=b
-        update(0,n-1,1,a-1,diff)
-        print('update: ',tree)
+        update(0,n-1,1,a-1,u*b+v)
+        # for i in tree:
+        #     print(i)
