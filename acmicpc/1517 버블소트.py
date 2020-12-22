55f0771da8fe08fc3e13e819cff7e19163e70c80
CODe1995 <code.57x53@gmail.com>
2020-12-23 00:38:25 +0900

    AC

--- acmicpc/1517 버블소트.py
+++ acmicpc/1517 버블소트.py
@@ -4,33 +4,39 @@ def ii():return int(input())
 def mii():return map(int,input().rstrip().split())
 def lmii():return list(map(int,input().rstrip().split()))
 
-arr = list()
-tree = list()
 N=0
-def query(index):    
+arr = list()
+fenwick = list()
+#fenwick을 이용해 값을 구하는 함수
+def query(index):
+    # print('[Query] index({}) fenwick : {}'.format(index,fenwick))
     ret = 0
-    while index:        
-        ret += tree[index]
-        # index &= (index-1)
-        index -= (index&-index)
+    while index>0:
+        ret+=fenwick[index]
+        index -= index&-index
+    # print('[Query] fenwick : {}, ret : {}'.format(fenwick,ret))
     return ret
 def update(index):
+    # print('[Update] index(%d) before update :'%(index),fenwick)
     while index<=N:
-        tree[index]+=1        
-        index+=(index&-index)
+        fenwick[index]+=1
+        index += index&-index
+    # print('[Update] index(%d) after update :'%(index),fenwick)
+
 if __name__ == "__main__":
     N = ii()
-    arr = lmii()#원본 배열
-    tree = [0]*(N+1)
-
-    for i,num in enumerate(arr):
-        arr[i] = [num,i]
-    print(arr)
-    #index 역순으로 정렬
-    arr=sorted(arr,key=lambda x:x[1],reverse=True)
-    print(arr)
+    arr= [0]*N
+    fenwick = [0]*(N+1)
+    tmp = lmii()#원본 배열
+    for idx,num in enumerate(tmp):
+        arr[idx] = [num,idx]
+    # for i in arr:
+    #     print(i)
+    arr.sort()
     ans = 0
-    for i in range(N):
-        ans+=query(arr[i][0]-1)
-        update(arr[i][0])
+    for a,b in arr:
+        # print("Query({})".format(b))
+        ans += query(N)-query(b)
+        # print("Update({})".format(b+1))
+        update(b+1)
     print(ans)
