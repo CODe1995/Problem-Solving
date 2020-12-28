62d38116b42066e56d5b2c34304420ef4b479271
CODe1995 <code.57x53@gmail.com>
2020-12-28 23:08:36 +0900

    메모리초과

---
+++ acmicpc/1976 여행 가자.py
@@ -0,0 +1,39 @@
+##########################################################
+import sys
+input = sys.stdin.readline
+def ip():return input().rstrip()
+def lip():return list(input().rstrip())
+def ii():return int(input())
+def mii():return map(int,input().rstrip().split())
+def lmii():return list(map(int,input().rstrip().split()))
+##########################################################
+sys.setrecursionlimit(10**7)
+n=ii()
+m=ii()
+parent = [i for i in range(n+1)]
+def find(x):
+    if parent[x]==x:return x
+    parent[x] = find(parent[x])
+    return parent[x]
+def union(a,b):
+    a = find(a)
+    b = find(b)
+    if a<b:parent[b]=a
+    else:parent[a]=b
+def check(a,b):
+    a=find(a)
+    b=find(b)
+    if a==b:return 1
+    return 0
+
+for i in range(1,n+1):
+    tmp = lmii()
+    for j in range(1,n+1):
+        if tmp[j-1]==1:
+            union(i,j)
+quest = lmii()
+for i in range(1,m):
+    if check(quest[i-1],quest[i])==0:
+        print('NO')
+        sys.exit()
+print('YES')
