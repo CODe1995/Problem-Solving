4f77701f9d03982174d5719bf5c9ed410e1c1d41
unknown <code.57x53@gmail.com>
2021-01-01 23:41:07 +0900

    AC

--- acmicpc/2206 벽 부수고 이동하기.py
+++ acmicpc/2206 벽 부수고 이동하기.py
@@ -17,20 +17,24 @@ for _ in range(h):
 
 def bfs():
     q = deque()
-    #X, Y, 벽부순 갯수, 이동거리
-    q.append([0,0,0,1])
+    visited = [[[0]*2 for _ in range(w)] for _ in range(h)]
+    visited[0][0][0]+=1 #첫 시작점은 1
+    #X, Y, 벽 부순 횟수, 방문지
+    q.append([0,0,0])
     while q:
-        x,y,broken,move=q.popleft()        
+        x,y,broken=q.popleft()
+        if x==w-1 and y==h-1:
+            return visited[y][x][broken]
         for dx,dy in direction:
             nx,ny = dx+x,dy+y
             if 0<=nx<w and 0<=ny<h:
-                if field[ny][nx]=='0':
-                    q.append([nx,ny,broken,move+1])
-                elif field[ny][nx]=='1' and broken==0:
-                    q.append([nx,ny,broken+1,move+1])                    
-                if [nx,ny]==[w-1,h-1]:
-                    return move+1
-
+                if visited[ny][nx][broken]==0 and field[ny][nx]=='0':#아직 방문 안했고, 0
+                    visited[ny][nx][broken]=visited[y][x][broken]+1
+                    q.append([nx,ny,broken])
+                elif visited[ny][nx][broken]==0 and field[ny][nx]=='1' and broken==0: #아직 방문 안했고, 1, 벽 안부쉈다면
+                    visited[ny][nx][broken+1]=visited[y][x][broken]+1
+                    q.append([nx,ny,broken+1])
+    return -1
 res = bfs()
 if res:print(res)
 else:print(-1)
