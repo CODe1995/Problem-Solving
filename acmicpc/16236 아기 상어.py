6d9063c62c708143c5eb22aa711a9aee3e0e214e
unknown <code.57x53@gmail.com>
2021-01-03 15:36:53 +0900

    AC

--- acmicpc/16236 아기 상어.py
+++ acmicpc/16236 아기 상어.py
@@ -1,5 +1,7 @@
 ##########################################################
 import sys
+from collections import deque
+direction = [[0,1],[-1,0],[1,0],[0,-1]] #for BFS
 input = sys.stdin.readline
 def ip():return input().rstrip()
 def lip():return list(ip())
@@ -8,67 +10,74 @@ def ii():return int(input())
 def mii():return map(int,ips())
 def lmii():return list(mii())
 ##########################################################
-# 초기 아기 상어 크기 : 2
-# 자기보다 작은 크기는 먹을 수 있음
-# 같은 크기는 지나갈 수 있음
-# 큰 크기는 지나가는 것도 안됌.
-# 본인 크기의 물고기 수를 먹으면 크기 증가.
-# 거리 결정 순서 : 
-#   1. 가장 가까움
-#   2. 가장 위
-#   3. 가장 왼쪽
-from collections import deque
-direction = [[0,1],[-1,0],[1,0],[0,-1]]
 n = ii()
 field = list()
 root = [-1,-1]
+def bfs():
+    q = deque([[root[0],root[1]]])
+    visited = [[root[0],root[1]]]
+    answer = 0
 
-def bfs(x,y,size,feed,time):
-    q = deque()
-    visited = [[[0,0,0] for _ in range(n)] for _ in range(n)]
-    eat = set()
-    ex,ey=-1,-1
-    #x좌표, y좌표, 상어 현재 크기, 먹은 물고기 수, 움직인 수
-    q.append([x,y,size,feed,time])    
+    shark_size = 2 # 초기 상어의 크기
+    shark_feed = 0 # 상어가 먹은 먹이의 수
+    shark_time = 0 # 상어가 움직인 시간(칸당 1 증가)
+
+    eat_flag = False # 먹이를 먹었는지 체크
+    # 먹이를 먹었다는 뜻은 가장 위 또는 좌측에 있는 먹이를 먹었으므로
+    # 더 이상 반복문(for _ in range(qSize))을 돌릴 필요가 없다는 뜻이다.
+    # 해당 반복문을 break하기 위한 용도로 쓰인다.
+
     while q:
-        x,y,size,feed,time = q.popleft()   
-        visited[y][x]=[size,feed,time]
-        for dx,dy in direction:
-            nx,ny = x+dx,y+dy
-            if 0<=nx<n and 0<=ny<n and visited[ny][nx][2]<=time:#범위체크
-                if 0<field[ny][nx]<size:#이동&포식
-                    q.append([nx,ny,size,feed+1,time+1])
-                    visited[ny][nx]=[size,feed+1,visited[y][x][2]+1]
-                    eat.add((ny,nx))
-                elif (field[ny][nx]==0 or field[ny][nx]==size) and visited[ny][nx][0:1]!=visited[y][x][0:1]:#이동
-                    q.append([nx,ny,size,feed,time+1])
-                    visited[ny][nx]=[size,feed,visited[y][x][2]+1]     
-        if not eat and not q:#아예 못움직이는 상황
-            print(visited[ey][ex][2])
-            sys.exit()
-        elif eat:
-            ey,ex = list(eat)[0]
-            eat.clear()
-            q.clear()
-            size,feed = visited[ey][ex][0],visited[ey][ex][1]
-            if size==feed:
-                size+=1
-                feed=0
-                visited[ey][ex]=[size,feed,visited[ey][ex][2]]
-            field[ey][ex]=0
-            q.append([ex,ey,size,feed,visited[ey][ex][2]])        
+        # 위쪽이 우선, 그 다음 왼쪽이 우선 정렬되어야 하므로
+        # y좌표(k[1])를 우선 정렬후 x좌표(k[0])를 기준으로 정렬한다.
+        if q:q = deque(sorted(q,key=lambda k:[k[1],k[0]]))
         
-        
-    print('done')
-    
+        qSize = len(q)
+        for _ in range(qSize):
+            x,y = q.popleft()
+            
+            #먹을 수 있는 범위
+            if 0<field[y][x]<shark_size:
 
+                field[y][x] = 0 #먹이를 먹었으므로 0으로 변경
+                shark_feed+=1   #먹은 먹이 수 증가
+                if shark_feed == shark_size:#크기 증가
+                    shark_size+=1
+                    shark_feed=0
+                # 먹이를 먹었으므로 새로운 탐색을 해야한다.
+                # 그래서 dq와 visited를 초기화 해준다.
+                q=deque()
+                visited = [[x,y]]
+                answer = shark_time
+                eat_flag=True
+
+            for dx,dy in direction:
+                nx,ny = x+dx,y+dy
+
+                # 필드 벗어나는 범위 체크, 방문 여부 체크
+                if 0<=nx<n and 0<=ny<n and not [nx,ny] in visited:
+                    # 이동 가능한 범위
+                    if field[ny][nx]<=shark_size:
+                        q.append([nx,ny])
+                        visited.append([nx,ny])
+            
+            if eat_flag:
+                eat_flag=False
+                break
+        shark_time+=1
+    return answer
+
+# 필드를 입력받고
+# 동시에 상어의 위치를 찾고 저장한다.
 for i in range(n):
     tmp = lmii()
     field.append(tmp)
     if [-1,-1]==root:
         for j in range(n):
-            if tmp[j]==9:#상어위치
+            if tmp[j]==9:
+                # 상어를 찾았다면 0으로 초기화 해주고
+                # 위치를 root에 등록해 준다.
                 root=[j,i]                
-                field[i][j]=0#루트 찾았으니까 초기화
-    
-print(bfs(root[0],root[1],2,0,0))
+                field[i][j]=0    
+print(bfs())
+# 소스코드 https://dailyheumsi.tistory.com/59 참고
