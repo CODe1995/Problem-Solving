5c11d5921d2d4d49c5f1613c07a4c1c9c9ca3824
CODe1995 <code.57x53@gmail.com>
2021-01-20 15:25:30 +0900

    AC py,java

---
+++ acmicpc/10808 알파벳 개수/Main.java
@@ -0,0 +1,16 @@
+package boj10808;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String s = sc.next();
+		int[] arr = new int[26];
+		for(int i = 0 ; i<s.length();i++)
+			arr[(int)s.charAt(i)-97]++;
+		for(int x:arr)
+			System.out.print(x+" ");
+		sc.close();
+	}
+}
