f0fbd3ea67bfc78cc3fab11f414034660fefcee8
CODe1995 <code.57x53@gmail.com>
2021-02-04 14:30:48 +0900

    17298 오큰수 python AC

--- acmicpc/17298 오큰수/17298.py
+++ acmicpc/17298 오큰수/17298.py
@@ -3,25 +3,12 @@ input = sys.stdin.readline
 N = int(input())
 arr = list(map(int,input().rstrip().split()))
 answer = [-1]*N
+stack = list()
 def solve():
-    maxnum = 0
-    for i in range(len(arr)-1,-1,-1):
-        if maxnum<arr[i]:
-            answer[i]=-1#제일 큰 수보다 크다면
-            maxnum = arr[i]
-        else:
-            if N>i+1:#범위체크
-                if arr[i]<arr[i+1]:#다음 값보다 내가 작다면
-                    answer[i] = arr[i+1]
-                else:#다음 값보다 내가 크다면
-                    for j in range(i+1,N):
-                        if arr[j]>arr[i]:
-                            answer[i]=arr[j]
-                            break
-                        if answer[j]>arr[i]:
-                            answer[i]=answer[j]
-                            break
-
+    for i in range(len(arr)):
+        while(len(stack)>0 and arr[stack[-1]]<arr[i]):
+            answer[stack.pop()] = arr[i]
+        stack.append(i)
 if __name__=="__main__":
     solve()
     for i in answer:
