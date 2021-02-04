0e1c24f289bf3bdf95895ce478cb60de206fc7c8
CODe1995 <code.57x53@gmail.com>
2021-02-05 00:30:56 +0900

    2263 트리 python3 AC 코드

--- acmicpc/2263 트리/2263.py
+++ acmicpc/2263 트리/2263.py
@@ -2,16 +2,17 @@ import sys
 input = sys.stdin.readline
 sys.setrecursionlimit(10**6)
 N = int(input())
-inorder = list(map(int,input().strip().split()))
-postorder = list(map(int,input().strip().split()))
-pos = {}
+inorder = list(map(int,input().split()))
+postorder = list(map(int,input().split()))
+pos = [0]*(N+1)
 for i in range(N):
     pos[inorder[i]]=i
 #VLR - preorder
 def solution(inStart,inEnd,postStart,postEnd):
     if inStart>=inEnd or postStart>=postEnd:return
     root = postorder[postEnd-1]
-    sys.stdout.write(str(root)+" ")#V
+    sys.stdout.write(str(root)+" ")
+    # print(root,end=" ")
     mid=pos[root]
     leftsize = mid-inStart
     solution(inStart,mid,postStart,postStart+leftsize)#L
