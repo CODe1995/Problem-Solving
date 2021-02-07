956cbdaf117e190a7a4092a6c2a319168f0df84d
CODe1995 <code.57x53@gmail.com>
2021-02-07 15:47:30 +0900

    15665 N과M(11) 20mins java AC

---
+++ acmicpc/15665 N과 M (11)/Main.java
@@ -0,0 +1,44 @@
+package boj15665;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.StringTokenizer;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	static StringBuilder sb = new StringBuilder();
+	static int N,M;
+	static int[] arr,newarr;
+	static void solution(int depth) {
+		if(depth==M) {
+			for (int i = 0; i < M; i++) {
+				sb.append(newarr[i]).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}		
+		int prev = -1;
+		for(int i =0;i<N;i++) {
+			if(prev==arr[i])continue;
+			prev = arr[i];
+			newarr[depth]=arr[i];
+			solution(depth+1);
+		}		
+	}
+	public static void main(String[] args) throws IOException{
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		arr = new int[N];
+		newarr = new int[M];
+		st = new StringTokenizer(br.readLine());
+		for(int i =0;i<N;i++)arr[i] = Integer.parseInt(st.nextToken());
+		Arrays.sort(arr);
+		solution(0);
+		System.out.println(sb);
+	}
+}
