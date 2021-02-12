bd966131dd2da6a0452da64e0470a35bc4e035e5
CODe1995 <code.57x53@gmail.com>
2021-02-13 01:20:35 +0900

    16954 움직이는미로탈출 95mins pypy3 2WA

---
+++ acmicpc/16954 움직이는 미로 탈출/16954.py
@@ -0,0 +1,18 @@
+import sys
+from collections import deque
+field = [['.']*8 for _ in range(8)]+[list(input()) for _ in range(8)]
+visited = [[0]*8 for _ in range(16)]
+direction = [[0,-1],[-1,-1],[1,-1],[1,0],[-1,0],[0,0]]
+dq = deque([[0,15]])
+while dq:
+    x,y = dq.popleft()
+    for dx,dy in direction:
+        nx,ny = dx+x,dy+y#y값 턴마다 움직임
+        if 0<=nx<8 and 0<=ny<16 and field[ny][nx]=='.':
+            if nx==7 and ny==0:
+                print(1)
+                sys.exit()            
+            if ny==0 or field[ny-1][nx]=='#':continue
+            visited[ny-1][nx] = 1
+            dq.append([nx,ny-1])
+print(0)
