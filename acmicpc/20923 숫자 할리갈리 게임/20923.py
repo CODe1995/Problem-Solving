87a7c83dffd4f53e98b33a1802a8b4d6beb94a22
CODe1995 <code.57x53@gmail.com>
2021-03-04 21:25:50 +0900

    20923 숫자할리갈리게임 코드단축 pypy3

--- acmicpc/20923 숫자 할리갈리 게임/20923.py
+++ acmicpc/20923 숫자 할리갈리 게임/20923.py
@@ -2,46 +2,30 @@ import sys
 from collections import deque
 input = sys.stdin.readline
 N,M = map(int,input().split())
-dodq = deque()  #도도덱
-sudq = deque()  #수연덱
-do_grdq = deque()   #도도 그라운드
-su_grdq = deque()   #수연 그라운드
+deck = [deque(),deque()]
+ground = [deque(),deque()]
 for _ in range(N):
     a,b = map(int,input().split())
-    dodq.appendleft(a)
-    sudq.appendleft(b)
-gameTime = 0
-winFlag = -1#0 도도승, 1 수연승, 2 비김
-def initGround():#그라운드 초기화
-    su_grdq=deque()
-    do_grdq=deque()
-while M!=gameTime:#M번 진행
-    if gameTime%2==0:#도도 턴
-        do_grdq.appendleft(dodq.popleft())#left에 넣어주면 reverse를 할 필요가 없어짐
-    else:#수연 턴
-        su_grdq.appendleft(sudq.popleft())
-
-    #덱 비었는지 체크
-    if not dodq or not sudq:
-        break
-
-    #그라운드 안비어있고 합 5
+    deck[0].appendleft(a)
+    deck[1].appendleft(b)
+turn = 0
+for _ in range(M):#M번 진행
+    ground[turn].appendleft(deck[turn].popleft())    
+    if not deck[turn]:break
     win = -1
-    if su_grdq and do_grdq and su_grdq[0]+do_grdq[0]==5:#수연종
-        win = 1
-    if (su_grdq and su_grdq[0]==5) or (do_grdq and do_grdq[0]==5):#도도종   
-        win = 2
-    if win!=-1:#승부가 난 경우 덱의 이동
-        if win==1:
-            while do_grdq:sudq.append(do_grdq.pop())
-            while su_grdq:sudq.append(su_grdq.pop())
-        elif win==2:
-            while su_grdq:dodq.append(su_grdq.pop())
-            while do_grdq:dodq.append(do_grdq.pop())
-    gameTime+=1
+    
+    for i in [0,1]:
+        if (ground[i] and ground[i][0]==5):win = 0
+    if ground[0] and ground[1] and ground[0][0]+ground[1][0]==5:win = 1
+
+    if win!=-1:
+        for i in [1-win,win]:
+            while ground[i]:
+                deck[win].append(ground[i].pop())
+    turn = 1-turn
 
 # print(dodq,sudq)
-if len(dodq)>len(sudq):winFlag=0
-elif len(dodq)<len(sudq):winFlag=1
+if len(deck[0])>len(deck[1]):winFlag=0
+elif len(deck[0])<len(deck[1]):winFlag=1
 else:winFlag=2
 print(['do','su','dosu'][winFlag])
