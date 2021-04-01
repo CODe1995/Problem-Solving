23166fa3912f02d2993e4177f1310a76c172987d
CODe1995 <code.57x53@gmail.com>
2021-04-01 23:20:23 +0900

    2304 창고다각형 pypy3 5WA

--- acmicpc/2304 창고 다각형/2304.py
+++ acmicpc/2304 창고 다각형/2304.py
@@ -6,18 +6,21 @@ arr.sort()
 pos = 1
 # print(pos,arr)
 while pos<len(arr)-1:
-    if arr[pos][1]<arr[pos-1][1]:
+    if arr[pos][1]<arr[pos-1][1] and arr[pos][1]<=arr[pos+1][1]:
         arr.pop(pos)
-        print(pos,arr)
+        # print(pos,arr)    
     else:pos+=1
 answer =0
-for i in range(len(arr)-1):
-    if i==len(arr)-2:#뒤에서 두번째
-        if arr[i][1]<arr[i+1][1]:
-            answer+=(arr[i+1][0]-arr[i][0]) * arr[i][1] + arr[i+1][1]
+for i in range(len(arr)):
+    if i!=len(arr)-1:
+        if arr[i][1]<=arr[i+1][1]:
+            answer+=(arr[i+1][0]-arr[i][0])*arr[i][1]
         else:
-            answer+=arr[i][1] + (arr[i+1][0]-arr[i][0])*arr[i+1][1]
+            answer+=arr[i][1]
     else:
-        answer+=(arr[i+1][0]-arr[i][0]) * arr[i][1]
+        if arr[i][1] < arr[i-1][1]:
+            answer+=(arr[i][0]-arr[i-1][0])*arr[i][1]
+        else:
+            answer+=arr[i][1]            
 
 print(answer)
