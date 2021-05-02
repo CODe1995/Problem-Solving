8363bb70e33df6a6db5c2906c730b2e14a0f85b4
CODe1995 <code.57x53@gmail.com>
2021-05-02 15:17:47 +0900

    11503 LIS 3mins pypy3 AC

---
+++ acmicpc/11053 가장 긴 증가하는 부분 수열/11503.py
@@ -0,0 +1,11 @@
+N = int(input())
+arr = list(map(int,input().split()))
+length = [1]*N
+maxLength = 1
+for i in range(N):
+    for j in range(i):
+        if arr[i]>arr[j] and length[i]<length[j]+1:
+            length[i]=length[j]+1
+            maxLength = max(maxLength,length[i])
+
+print(maxLength)
