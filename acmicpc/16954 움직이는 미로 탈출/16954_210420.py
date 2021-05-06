ee454ba22cbf103aa6c15ca6168d775819f69021
CODe1995 <code.57x53@gmail.com>
2021-05-06 23:19:13 +0900

    16954 움직이는 미로탈출 pypy3 AC

--- acmicpc/16954 움직이는 미로 탈출/16954_210420.py
+++ acmicpc/16954 움직이는 미로 탈출/16954_210420.py
@@ -2,15 +2,11 @@ from collections import deque
 MAX = 8
 direction = [[0,0],[0,-1],[0,1],[-1,0],[1,0],[-1,-1],[1,-1],[1,1],[-1,1]]
 field=[list(input()) for _ in range(MAX)]
-visited=[['.']*MAX for _ in range(MAX)]
+visited=[[0]*MAX for _ in range(MAX)]
 dq = deque([[0,7,0]])
 answer = 0
 while dq:
     cur = dq.popleft()
-    visited[cur[1]][cur[0]]='@'
-    if cur[1]==0 or cur[2]==7:
-        answer=1
-        break        
     for dx,dy in direction:
         nx = cur[0]+dx
         ny = cur[1]+dy
@@ -19,7 +15,9 @@ while dq:
             dq.clear()
             answer=1
             break
+        if visited[ny][nx]>=cur[2]+1:continue
         if field[ny-cur[2]][nx]=='#':continue        
         if field[ny-(cur[2]+1)][nx]=='#':continue        
         dq.append([nx,ny,cur[2]+1])
+        visited[ny][nx]=cur[2]+1
 print(answer)
