3714d7d1f56b16feeff709722a5aaddf9b406f91
CODe1995 <code.57x53@gmail.com>
2021-05-14 23:35:57 +0900

    월코챌2021 5월 3번 110옮기기 greedy WA

--- programmers/algorithm/2021 월간 코드 챌린지2/3.py
+++ programmers/algorithm/2021 월간 코드 챌린지2/3.py
@@ -3,41 +3,38 @@ def solution(s):
     answer = []
     
     for cur in s:
-        visited = dict()
-        nextIdx = 0
-        nextTIdx = 0
-        lcur = list(cur)
-        while True:
-            if cur in visited:
-                answer.append(cur)
-            visited[cur]=True            
-            tidx = cur.find('111',nextTIdx)  #옮겨야 하는 위치            
-            if tidx > -1:# 111이 있을때
-                idx = cur.find('110',nextIdx)   #110 위치
-                nextIdx = tidx+3
-                
-                if idx<tidx:#111이 뒤에 있는 경우? = 바꾸면 손해임
-                    break
-                if idx==-1 or nextIdx>=len(cur):# 110 없다면?
-                    break
-
-                nextTIdx = tidx+3
-                #-- 위치 swap
-                lcur[idx]='1'
-                lcur[idx+1]='1'
-                lcur[idx+2]='1'
-                lcur[tidx]='1'
-                lcur[tidx+1]='1'
-                lcur[tidx+2]='0'
-                #--
-                cur = ''.join(lcur)
-                
-
-            else:   #옮길 필요가 없음
-                break    
-        answer.append(cur)
+        stack = list()
+        cnt = 0 #뽑아낸 110의 갯수        
+        for i in range(len(cur)):
+            stack.append(cur[i])
+            if len(stack)>=3:
+                temp = stack[-3:]                
+                if temp == ['1','1','0']:
+                    cnt+=1
+                    stack.pop()
+                    stack.pop()
+                    stack.pop()
+        
+        for i in range(len(stack)):
+            if len(stack)>=3 and stack[i:i+3]==['1','1','1']:#최초 111
+                for j in range(cnt):    #110을 뽑은 갯수만큼 삽입
+                    stack.insert(i,'0')
+                    stack.insert(i,'1')
+                    stack.insert(i,'1')
+                break
+        else:   #111이 존재하지 않는다면?
+            for j in range(cnt):    #110을 뽑은 갯수만큼 삽입
+                stack.insert(i,'0')
+                stack.insert(i,'1')
+                stack.insert(i,'1')
+                # stack.append('1')
+                # stack.append('1')
+                # stack.append('0')        
+        # print(stack,cnt)
+        answer.append(''.join(stack))
     return answer
 
 print(solution(['1110110110110','1110','100111100','0111111010','1111','11100']))
 # print(solution(['0111111010']))
+# print(solution(['10111111100']))
 #1101 100110110 0110110111
