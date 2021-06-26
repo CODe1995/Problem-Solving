1481acfbf18f1771bd7bcb9995c0d741edc50bf8
CODe1995 <code.57x53@gmail.com>
2021-06-27 00:31:58 +0900

    2156 포도주서식 dp 15mins pypy3 2RTE AC

--- acmicpc/2156 포도주 시식/2156_포도주시식.py
+++ acmicpc/2156 포도주 시식/2156_포도주시식.py
@@ -8,11 +8,11 @@ dp = [0]*N
 # N, N-2, N-3
 
 dp[0] = arr[0]  #첫번째 잔 선택
-dp[1] = arr[0]+arr[1]   #첫번째 두번째 선택
-#첫번째 세번째, 두번째 세번째, 첫번째 두번째 중  최댓값 선택
-dp[2] = max(arr[2]+arr[0],arr[2]+arr[1],dp[1])
-for i in range(3,N):
-    dp[i] = arr[i] + max(dp[i-3]+arr[i-1],dp[i-2])
-    dp[i] = max(dp[i],dp[i-1])
 
-print(dp[N-1])
+if N>1:dp[1] = arr[0]+arr[1]   #첫번째 두번째 선택
+#첫번째 세번째, 두번째 세번째, 첫번째 두번째 중  최댓값 선택
+if N>2:dp[2] = max(arr[2]+arr[0],arr[2]+arr[1],dp[1])
+for i in range(3,N):
+    dp[i] = max(arr[i] + dp[i-3]+arr[i-1],arr[i] + dp[i-2], dp[i-1])
+
+print(max(dp))
