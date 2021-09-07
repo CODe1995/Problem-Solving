9f9a906ff58279de5766286ac46fc305c752b423
CODe1995 <code.57x53@gmail.com>
2021-09-08 02:35:22 +0900

    12100 2048(Easy) 시간&코드단축 pypy3 3200b->1600b
    1. 동서남북으로 나누어 구현했던 로직을 동서, 남북으로 분할함
    2. 이 과정에서 condition 변수를 통해 for문의 조건을 제어함
    3. 이후 x,y만 스왑하면 된다는 규칙을 파악하고  동서남북을 한번에 처리함

--- acmicpc/12100 2048(Easy)/12100.py
+++ acmicpc/12100 2048(Easy)/12100.py
@@ -5,7 +5,6 @@ field = [list(map(int,input().split())) for _ in range(N)]
 direction = [[0,-1],[1,0],[0,1],[-1,0]] # 북동남서
 answer = 0
 
-
 def GetMaxBlock(temp):
     mx = 0
     for i in range(N):
@@ -15,81 +14,35 @@ def GetMaxBlock(temp):
 
 # 돌을 방향대로 움직임 (0-4,북동남서)
 def MoveBlock(temp,d):
-    #북
     newtemp = deepcopy(temp)
     dq = deque()    
-    if d==0:
-        for x in range(N):     
-            for y in range(N):
-                if newtemp[y][x]==0:
-                    continue
+    for x in range(N):     
+        condition = range(N) if d in [0,3] else range(N-1,-1,-1)
+        tx,ty = 0,0
+        for y in condition:  # 0 to N-1 or N-1 to 0
+            if d in [1,3]:
+                tx,ty=y,x
+            else:
+                tx,ty=x,y
+            if newtemp[ty][tx]==0:
+                continue
+            else:
+                dq.append(newtemp[ty][tx])
+                newtemp[ty][tx]=0
+        for i in range(1,len(dq)):
+            if dq[i-1]==dq[i] and dq[i]!=0:
+                dq[i-1]*=2   # 앞으로 합치고
+                dq[i]=0      # 뒤를 비움
+        idx = 0 if d in [0,3] else N-1
+        while dq:
+            nxt = dq.popleft()
+            if nxt != 0:
+                if d in [0,2]:
+                    newtemp[idx][tx] = nxt
                 else:
-                    dq.append(newtemp[y][x])
-                    newtemp[y][x]=0
-            for i in range(1,len(dq)):
-                if dq[i-1]==dq[i] and dq[i]!=0:
-                    dq[i-1]*=2   # 앞으로 합치고
-                    dq[i]=0      # 뒤를 비움
-            idx = 0
-            while dq:
-                nxt = dq.popleft()
-                if nxt != 0:
-                    newtemp[idx][x] = nxt
-                    idx+=1
-    elif d==2:
-        for x in range(N):
-            for y in range(N-1,-1,-1):
-                if newtemp[y][x]==0:
-                    continue
-                else:
-                    dq.append(newtemp[y][x])
-                    newtemp[y][x]=0
-            for i in range(1,len(dq)):
-                if dq[i-1]==dq[i] and dq[i]!=0:
-                    dq[i-1]*=2   # 앞으로 합치고
-                    dq[i]=0      # 뒤를 비움
-            idx = N-1
-            while dq:
-                nxt = dq.popleft()
-                if nxt != 0:
-                    newtemp[idx][x] = nxt
-                    idx-=1
-    elif d==1:#동
-        for y in range(N):
-            for x in range(N-1,-1,-1):
-                if newtemp[y][x]==0:
-                    continue
-                else:
-                    dq.append(newtemp[y][x])
-                    newtemp[y][x]=0
-            for i in range(1,len(dq)):
-                if dq[i-1]==dq[i] and dq[i]!=0:
-                    dq[i-1]*=2   # 앞으로 합치고
-                    dq[i]=0      # 뒤를 비움
-            idx = N-1
-            while dq:
-                nxt = dq.popleft()
-                if nxt != 0:
-                    newtemp[y][idx] = nxt
-                    idx-=1
-    elif d==3:#서
-        for y in range(N):
-            for x in range(N):
-                if newtemp[y][x]==0:
-                    continue
-                else:
-                    dq.append(newtemp[y][x])
-                    newtemp[y][x]=0
-            for i in range(1,len(dq)):
-                if dq[i-1]==dq[i] and dq[i]!=0:
-                    dq[i-1]*=2   # 앞으로 합치고
-                    dq[i]=0      # 뒤를 비움
-            idx = 0
-            while dq:
-                nxt = dq.popleft()
-                if nxt != 0:
-                    newtemp[y][idx] = nxt
-                    idx+=1
+                    newtemp[ty][idx] = nxt
+                if d in [0,3]:idx+=1
+                else: idx-=1
     return newtemp
 
 def dfs(depth,temp):
