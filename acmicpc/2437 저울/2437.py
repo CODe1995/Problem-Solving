a1a15efe4743f542838ea37b9ff088ce2b11ecaf
CODe1995 <code.57x53@gmail.com>
2022-01-08 21:20:27 +0900

    boj2437 greedy pypy Ac

--- acmicpc/2437 저울/2437.py
+++ acmicpc/2437 저울/2437.py
@@ -1,22 +1,10 @@
 N = int(input())
 weightList = list(map(int,input().split()))
-possibleWeight = dict()
-answer = 1000000*1000+1
+weightList.sort()
+sumWeight = 0
 for i in range(N):
     w = weightList[i]
-    keys = list(possibleWeight.keys())
-    for pw in keys:
-        nextWeight = pw+w
-        if nextWeight not in possibleWeight:
-            possibleWeight[nextWeight] = 1
-        nextWeight = pw-w
-        if nextWeight <= 0:
-            continue        
-        if nextWeight not in possibleWeight:
-            possibleWeight[nextWeight] = 1
-    if w not in possibleWeight:
-        possibleWeight[w] = 1
-for i in range(1,1000000*1000+1):
-    if i not in possibleWeight:
-        print(i)
+    if sumWeight+1 < w:
         break
+    sumWeight += w
+print(sumWeight+1)
