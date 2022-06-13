68801cdd22b569cc71c4fdf0068e91d11856aa21
CODe1995 <code.57x53@gmail.com>
2022-06-13 23:23:32 +0900

    [prgm] 최소직사각형 py3 AC

---
+++ programmers/algorithm/위클리 챌린지/최소직사각형.py
@@ -0,0 +1,8 @@
+def solution(sizes):
+    maxW = 0
+    maxH = 0
+    for w, h in sizes:
+        maxW = max(maxW, max(w,h))
+        maxH = max(maxH, min(w,h))
+    answer = maxW * maxH
+    return answer
