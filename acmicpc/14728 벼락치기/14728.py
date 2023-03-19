7ebb976f83a8c0b51a5f5c4b0d48c06b4bdd6bb7
CODe1995 <code.57x53@gmail.com>
2023-03-19 23:20:49 +0900

    [boj] 14728 벼락치키 knapsack,dp py AC

---
+++ acmicpc/14728 벼락치기/14728.py
@@ -0,0 +1,9 @@
+N, T = map(int, input().split())
+lists = [list(map(int, input().split())) for _ in range(N)]
+dp = [0] * (T+1)
+
+for cost, weight in lists:
+    for j in range(T, cost, -1):
+        dp[j] = max(dp[j - cost] + weight, dp[j])
+
+print(dp[T])
